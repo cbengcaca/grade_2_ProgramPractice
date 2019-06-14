9ad7d1466e8226010adfb95dda1f521f63c1e8b9
WeiSheng Huang <48779061+XBa1@users.noreply.github.com>
2019-06-14 15:54:22 +0800

    Version2
    Borrow OR Return Book

--- 黄惟盛/V_BorrowOrReturnBook.py
+++ 黄惟盛/V_BorrowOrReturnBook.py
@@ -4,18 +4,22 @@ import tkinter as tk
 import tkinter.messagebox
 import datetime
 
-class V_BorrowBook:
+class V_BorrowOrReturnBook:
 
-    def __init__(self):
+    def __init__(self,type):                #type等于0为借书，type等于1为还书
         self.__root = Tk()
-        self.__root.title("借书")
+        self.__type = type;
+        if type==0:
+            self.__root.title("借书")
+        elif type ==1:
+           self.__root.title("还书")
         self.__root.geometry('420x70')  # 借书窗口起始
 
         frm = Frame(self.__root)  # 主界面起始
         frm_F = Frame(frm)  # 第一层起始
 
         frm_FL = Frame(frm_F)  # 第一层左起始
-        Label(frm_FL, text='ISBN', font=('Arial', 12)).pack(side=TOP)
+        Label(frm_FL, text='书籍号', font=('Arial', 12)).pack(side=TOP)
         frm_FL.pack(side=LEFT)  # 第一层左放置
 
         frm_FR = Frame(frm_F)  # 第一层右起始
@@ -36,12 +40,16 @@ class V_BorrowBook:
 
     def ensure(self):
         isbn=self.__entryISBN.get()
+        if self.__type == 0:
+            1
+            #借书控制层书籍信息操作
+        elif self.__type == 1:
+            1
+            #还书控制层书籍信息操作
 
-        #控制层书籍信息操作
-
-        if 1:
+        if 1:       #操作成功
             a = tkinter.messagebox.showinfo('提示', '借书成功')
-        else:
+        else:       #操作失败
             a = tkinter.messagebox.showerror('错误', '借书失败')
 
-a= V_BorrowBook()
+a= V_BorrowOrReturnBook(1)
