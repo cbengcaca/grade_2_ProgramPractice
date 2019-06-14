e5da795210dbe6803e8f9cedef551e8cce1b7901
Laofuyihao <51694807+Laofuyihao@users.noreply.github.com>
2019-06-14 22:47:44 +0800

    啊！

--- 盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒/V_SearchBorrowMassage.py
+++ 盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒盒/V_SearchBorrowMassage.py
@@ -1,11 +1,11 @@
 from tkinter import *
 from tkinter import ttk
 
-class V_SearchReader:
+class V_SearchBorrowMassage:
 
     def __init__(self):
         self.__root = Tk()
-        self.__root.title('SearchReader')
+        self.__root.title('SearchBorrowMassage')
         self.__root.geometry('400x420')
         #self.__root.resizable(0,0)
 
@@ -14,25 +14,25 @@ class V_SearchReader:
         frameTreeView = Frame(self.__root)
         frameTreeView.pack(side=TOP)
 
-        labelReaderNumber = Label(frameLine, text='Input reader number:')
-        self.__stringVarReaderNumber = StringVar()
-        entryReaderNumber = Entry(frameLine, textvariable = self.__stringVarReaderNumber)
+        labelBorrowMassageNumber = Label(frameLine, text='Input borrow number:')
+        self.__stringVarBorrowMassageNumber = StringVar()
+        entryBorrowMassageNumber = Entry(frameLine, textvariable = self.__stringVarBorrowMassageNumber)
         buttonSearch = Button(frameLine, text='查询')
         buttonCancel = Button(frameLine, text='返回', command = self.__root.quit)
-        labelReaderNumber.pack(side = LEFT)
-        entryReaderNumber.pack(side = LEFT)
+        labelBorrowMassageNumber.pack(side = LEFT)
+        entryBorrowMassageNumber.pack(side = LEFT)
         buttonSearch.pack(side = LEFT)
         buttonCancel.pack(side = LEFT)
 
-        columns = ("读者号", "姓名", "联系方式", "是否逾期")
+        columns = ("借单号", "姓名", "联系方式", "是否逾期")
         tv = ttk.Treeview(frameTreeView, height=18, show="headings", columns=columns)
 
-        tv.column("读者号", width=90, anchor='center')
+        tv.column("借单号", width=90, anchor='center')
         tv.column("姓名", width=100, anchor='center')
         tv.column("联系方式", width=100, anchor='center')
         tv.column("是否逾期", width=100, anchor='center')
 
-        tv.heading("读者号", text="读者号")  # 显示表头
+        tv.heading("借单号", text="借单号")  # 显示表头
         tv.heading("姓名", text="姓名")
         tv.heading("联系方式", text="联系方式")
         tv.heading("是否逾期", text="是否逾期")
@@ -43,4 +43,4 @@ class V_SearchReader:
 
 
 
-a = V_SearchReader()
+a = V_SearchBorrowMassage()
