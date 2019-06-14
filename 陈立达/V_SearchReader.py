24b17d50a82f73b0297cd5723b1b652587bf1313
Laofuyihao <51694807+Laofuyihao@users.noreply.github.com>
2019-06-14 22:36:17 +0800

    ！！！

---
+++ 陈立达/V_SearchReader.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+class V_SearchReader:
+
+    def __init__(self):
+        self.__root = Tk()
+        self.__root.title('SearchReader')
+        self.__root.geometry('400x200')
+        self.__root.resizable(0,0)
