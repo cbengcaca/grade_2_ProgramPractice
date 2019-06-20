b4e1ec53a2e56ef83231e454e0a8fd6946d6da65
cbengcaca <50434136+cbengcaca@users.noreply.github.com>
2019-06-21 00:16:40 +0800

    陈立达
    TCP 服务端

---
+++ 陈立达/TCP_hoster/hoster.py
@@ -0,0 +1,127 @@
+from socket import *
+from tkinter import *
+from tkinter import messagebox
+import threading
+from M_DBOperation import M_DBOperation
+import time
+class TcpHoster(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.flag = True
+        self.host = '192.168.43.125'
+        self.port = 4700
+        self.buf = 1024
+        self.addr = (self.host, self.port)
+
+
+    def run(self):
+        s = socket(AF_INET, SOCK_STREAM, 0)
+        s.bind(self.addr)
+        s.listen(5)
+        while self.flag is not False:
+            print('等待连接...\r\n')
+            cs, caddr = s.accept()
+
+            if self.flag is False:
+                cs.close()
+                print('...服务器停止\r\n')
+                break
+            if caddr is not self.host:
+                print('...连接来自于:', caddr)
+                data = '欢迎你的到来！\r\n'
+                cs.sendall(bytes(data, 'utf-8'))
+
+            recKeyWords = []
+            while True:
+                data = cs.recv(self.buf).decode('utf-8')
+                print(data)
+                if data == '==':
+                    break
+                recKeyWords.append(data)
+            print(recKeyWords)
+            #searchRec = M_DBOperation(recKeyWords)
+
+            list = [
+                "1bookName,bookPrice,bookCreateTime,bookAuthor,bookPublisher,bookSaleNumber,shelfLocate",
+                "2bookName,bookPrice,bookCreateTime,bookAuthor,bookPublisher,bookSaleNumber,shelfLocate",
+                "3bookName,bookPrice,bookCreateTime,bookAuthor,bookPublisher,bookSaleNumber,shelfLocate",
+                "4bookName,bookPrice,bookCreateTime,bookAuthor,bookPublisher,bookSaleNumber,shelfLocate"
+            ]
+
+            for i in list:
+                cs.sendall(bytes(i, 'utf-8'))
+                time.sleep(0.1)
+            time.sleep(0.5)
+            cs.sendall(bytes('==', 'utf-8'))
+            #for singleLine in searchRec.selectBook():
+            #    singleLineChangeToStr = ','.join([str(i) for i in singleLine])
+            #    cs.sendall(bytes(singleLineChangeToStr,'utr-8'))
+            #cs.sendall(bytes('==', 'utf-8'))
+            cs.close()
+
+
+        return
+
+    def stopThread(self):
+        self.flag = False
+        return
+
+class hosterCtrl(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.hoster = None
+
+        self.root = Tk()
+        self.root.geometry('400x200')
+        self.root.geometry('+500+200')
+        self.root.resizable(0,0)
+
+        labelBlank1 = Label(self.root)
+        labelBlank1.pack(side = TOP)
+
+        buttonStartThread = Button(self.root,text = 'START HOSTER',font = 'Consoles',command = self.startHoster)
+        buttonStartThread.pack(side = TOP)
+
+        labelBlank2 = Label(self.root)
+        labelBlank2.pack(side = TOP)
+
+        buttonEndThread = Button(self.root ,text = 'END HOSTER',font = 'Consoles',command = self.stopHoster)
+        buttonEndThread.pack(side = TOP)
+
+        labelBlank3 = Label(self.root)
+        labelBlank3.pack(side = TOP)
+
+        buttonCloseSystem = Button(self.root,text = 'END THIS SYSTEM',font = 'Consoles',command = self.closeSystem)
+        buttonCloseSystem.pack(side = TOP)
+
+        mainloop()
+
+    def startHoster(self):
+        if self.hoster is not None:
+            messagebox.showwarning('tip', 'Hoster is already start')
+        else:
+            self.hoster = TcpHoster()
+            self.hoster.start()
+            messagebox.showinfo('tip', 'Hoster is start')
+
+    def stopHoster(self):
+        if self.hoster is None:
+            messagebox.showwarning('tip', 'Hoster is already stop')
+
+        else:
+            self.hoster.stopThread()
+            addr = (self.hoster.host, self.hoster.port)
+            cs = socket(AF_INET, SOCK_STREAM, 0)
+            cs.connect(addr)
+            self.hoster = None
+            messagebox.showinfo('tip', 'Hoster is stop')
+
+    def closeSystem(self):
+        if self.hoster is not None:
+            messagebox.showwarning('tip', 'Hoster is not stop')
+        else:
+            messagebox.showinfo('tip', 'System will close soon')
+            self.root.destroy()
+
+T_hosterCtrl = hosterCtrl()
+T_hosterCtrl.start()
---
+++ 陈立达/TCP_hoster/test.py
@@ -0,0 +1,7 @@
+data = ['a', '0', '0', '0', '0', 'None', 'None', 'None', '==']
+recKeyWords = []
+for i in data:
+    if data == '==':
+        pass
+    recKeyWords.append(i)
+print(recKeyWords)
