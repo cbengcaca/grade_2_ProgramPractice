5d5e42b098a5e761f6e0b677f6d0a6e54310bad2
cbengcaca <50434136+cbengcaca@users.noreply.github.com>
2019-06-30 15:48:52 +0800

    Update M_SqlRunner.py

--- Main_Project/M_Hoster/M_SqlRunner/M_SqlRunner.py
+++ Main_Project/M_Hoster/M_SqlRunner/M_SqlRunner.py
@@ -1,19 +1,21 @@
 import pymysql
 class M_SqlRunner:
-    def beginSql(self,sql):
-        tmp = sql.strip(' ').split()[0]
+    def beginSql(self,sqlList):
         ret = ''
-        db = pymysql.connect('106.52.87.149', 'root', '000000', 'softwarePractice', autocommit=True)
+        db = pymysql.connect('106.52.87.149', 'root', '000000', 'softwarePractice')
         cursor = db.cursor()
         try:
-            cursor.execute(sql)
-            if tmp is "select":
-                ret = cursor.fetchall()
-            else:
-                pass
+            for sql in sqlList:
+                tmp = sql.strip(' ').split()[0]
+                cursor.execute(sql)
+                if tmp is "select":
+                    ret = cursor.fetchall()
+            db.commit()
             db.close()
             print("operation done")
         except Exception as e:
+            db.rollback()
             print("error:unable to fetch data", e)
 
         return ret
+
