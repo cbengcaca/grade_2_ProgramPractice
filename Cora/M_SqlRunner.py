96560dbccc28c4a6e0f512859d805537734efc89
Deer-Cora <51710452+Deer-Cora@users.noreply.github.com>
2019-06-30 17:22:38 +0800

    Create M_SqlRunner.py

---
+++ Cora/M_SqlRunner.py
@@ -0,0 +1,23 @@
+import pymysql
+class M_SqlRunner:
+    def beginSql(self,sqlList):
+        ret = ''
+        db = pymysql.connect('106.52.87.149', 'root', '000000', 'softwarePractice')
+        cursor = db.cursor()
+        try:
+
+            for sql in sqlList:
+                tmp = sql.strip(' ').split()[0]
+                cursor.execute(sql)
+                if tmp == 'SELECT':
+                    db.commit()
+                    ret = cursor.fetchall()
+                    return ret
+            db.commit()
+            db.close()
+            print("operation done")
+        except Exception as e:
+            db.rollback()
+            print("error:unable to fetch data", e)
+
+        return ret
