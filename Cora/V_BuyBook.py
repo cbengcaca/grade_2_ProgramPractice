3d0cef751216446cf18a31a0d11155f95a4cb85d
Deer-Cora <51710452+Deer-Cora@users.noreply.github.com>
2019-07-02 17:24:04 +0800

    Update V_BuyBook.py

--- Cora/V_BuyBook.py
+++ Cora/V_BuyBook.py
@@ -15,7 +15,7 @@ class V_BuyBook:
         #self.__imagePay = ImageTk.PhotoImage(image=self.__payCode)
         self.__payCode = qrcode.make('success')
         self.__imagePay = ImageTk.PhotoImage(image=self.__payCode)
-
+        self.buyBook = C_BuyBook()
         frm = Frame(self.__root)
 
         frameLable = Frame(frm)                   #设置标签
@@ -31,41 +31,43 @@ class V_BuyBook:
         frm.pack(side = TOP, anchor = CENTER)
 
         frameButton = Frame(self.__root)          #设置按钮
-        Button(frameButton, text = '购买', command = self.payBook, width = 6, height = 1).pack(side = LEFT)     #弹出支付窗口
-        Button(frameButton, text = '取消', command = self.__root.quit, width = 6, height = 1).pack(side = LEFT) #返回
+        Button(frameButton, text = '购买', command = self.payBook, width = 6, height = 1).pack(side = LEFT)       #支付界面
+        Button(frameButton, text = '取消', command = self.__root.quit, width = 6, height = 1).pack(side = LEFT)   #返回
         frameButton.pack(anchor = CENTER)
 
         mainloop()
 
     # 创建付款子窗口
     def payBook(self):
-        top = Toplevel()             #创建顶级窗口
-        top.title('支付')
-        top.geometry('400x400')
-        top.resizable(0, 0)  # 不可重设窗口大小
-        msg = Message(top, text = '请扫描二维码付款', width = 100).pack(anchor = CENTER)
-        Label(top, image = self.__imagePay).pack(anchor = CENTER)
+        # top = Toplevel()             #创建顶级窗口
+        # top.title('支付')
+        # top.geometry('400x400')
+        # top.resizable(0, 0)  # 不可重设窗口大小
+        ret = self.buyBook.sendBuyMessage(self.getBookId(), 0)
+        if  ret == '2':
+            second = Toplevel()
+            msg = Message(second, text = '请扫描二维码付款', width = 100).pack(anchor = CENTER)
+            Label(second, image = self.__imagePay).pack(anchor = CENTER)
 
-        frameButton = Frame(top)
-        Button(frameButton, text = '我已付款', command = self.resultOfBuy, width = 6, height = 1).pack(side = LEFT)     #支付成功
-        Button(frameButton, text = '放弃购买', command = top.quit, width = 6, height = 1).pack(side = LEFT)
-        frameButton.pack(anchor = CENTER)
+            frameButton = Frame(second)
+            Button(frameButton, text = '我已付款', command = self.resultOfBuy, width = 6, height = 1).pack(side = LEFT)     #支付成功
+            Button(frameButton, text = '放弃购买', command = second.quit, width = 6, height = 1).pack(side = LEFT)
+            frameButton.pack(anchor = CENTER)
+        else:
+            messagebox.showwarning('错误', '书籍ID有误')
+            self.__root.quit()
+            self.__root.destroy()
 
     def getBookId(self):
         return self.__varBookId.get()
 
     def resultOfBuy(self):
-        # top = Toplevel()  # 创建顶级窗口
-        # top.title('购买结果')
-        # top.geometry('400x200')
-        # top.resizable(0, 0)  # 不可重设窗口大小
-        buyBook = C_BuyBook(self.getBookId())
-
-        if buyBook.sendMessage():
+        # self.buyBook = C_BuyBook(self.getBookId())
+        if self.buyBook.sendBuyMessage(self.getBookId(), 1) == '1':
             messagebox.showinfo('购买成功', '购买成功，谢谢惠顾。')
             self.__root.quit()
             self.__root.destroy()
-        else:
+        if self.buyBook.sendBuyMessage(self.getBookId(), 1) == '-1':
             messagebox.showwarning('购买失败', '请去前台处理。')
             self.__root.quit()
             self.__root.destroy()
