60cee4f1bf037fbc61b42c1090b40e2ea9e959c7
Deer-Cora <51710452+Deer-Cora@users.noreply.github.com>
2019-07-02 17:23:52 +0800

    Update C_BuyBook.py

--- Cora/C_BuyBook.py
+++ Cora/C_BuyBook.py
@@ -1,12 +1,19 @@
 from sendToServ import SendToServ
 
 class C_BuyBook:
-    def __init__(self, bookId):
-        self.__bookId = bookId
+    def __init__(self):
         self.__sendToServ = SendToServ()     #生成一个对象
 
     #向服务器发送购买书籍请求
-    def sendMessage(self):
-        dataList = ['2', self.__bookId, '==']
-        ret = self.__sendToServ.send(dataList, '192.168.56.1')
+    def sendBuyMessage(self, bookId, payFlag):
+        dataList = ['2']
+        dataList.append(str(bookId))
+        dataList.append(str(payFlag))
+        dataList.append('==')
+        ret = self.__sendToServ.send(dataList, '192.168.56.1')[0][0]
         return ret
+
+if __name__ == '__main__':
+    c = C_BuyBook()
+    ret = c.sendBuyMessage(12, 1)
+    print(ret)
