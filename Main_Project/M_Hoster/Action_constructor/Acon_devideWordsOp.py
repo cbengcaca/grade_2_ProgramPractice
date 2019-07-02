ce913c01d735b9c879486d6d2c3b1c570063fcdf
cbengcaca <50434136+cbengcaca@users.noreply.github.com>
2019-07-02 23:21:30 +0800

    完成了主页查书返回位置跟最早归还时间的操作

--- Main_Project/M_Hoster/Action_constructor/Acon_devideWordsOp.py
+++ Main_Project/M_Hoster/Action_constructor/Acon_devideWordsOp.py
@@ -10,23 +10,54 @@ class Acon_devideWordsOp:
         self.__bookPublisher = infoList[5]      #出版社
         self.__sortKey = infoList[6]            #排序关键字
         self.__sortFlag = infoList[7]           #升降序标志
-        self.__selSQL =  "SELECT bookName, bookPrice, bookCreateTime, bookAuthor, bookPublisher, bookSaleNumber, shelfLocate"
-        self.__selSQL += " FROM T_ISBNInfo, T_ISBNAndShelf, T_shelfLocate"
-        self.__selSQL += " WHERE T_ISBNInfo.ISBN =  T_ISBNAndShelf.ISBN AND T_shelfLocate.shelfId = T_ISBNAndShelf.shelfId AND T_ISBNInfo.bookMaxNum != 0"
-
+        self.__selSQL =  "SELECT a.bookName,a.bookPrice,a.bookCreateTime,a.bookAuthor, " \
+                         "a.bookPUblisher,a.bookAvailableNum ,a.bookSaleNumber, b.shelflocate, date_format(b.returntime, '%Y%m%d')"
+        self.__selSQL += " from t_isbninfo a left join v_borrowandbookinfo b on a.isbn = b.isbn"
+        #date_format(time, '%Y%m%d')
+        flagIfFirst = 0
+        previousNum = 0
         # 查找书名、价格、出版时间、作者、出版社、销量、位置
         if self.__bookName != 'None':  # 书名不为空，模糊查询
-            self.__selSQL += " AND bookName LIKE '%" + self.__bookName + "%'"
+            flagIfFirst += 1
+            previousNum += 1
+            if flagIfFirst is 1:
+                self.__selSQL += " Where "
+            if previousNum >= 2:
+                self.__selSQL += " AND "
+            self.__selSQL += "bookName LIKE '%" + self.__bookName + "%'"
         if self.__bookMinPrice != 'None':  # 所查找书籍的价格底线
-            self.__selSQL += "AND bookPrice > " + self.__bookMinPrice
+            flagIfFirst += 1
+            if flagIfFirst is 1:
+                self.__selSQL += " Where "
+            if previousNum >= 2:
+                self.__selSQL += " AND "
+            self.__selSQL += "bookPrice > " + self.__bookMinPrice
         if self.__bookMaxPrice != 'None':  # 所查找书籍的价格上限
-            self.__selSQL += " AND bookPrice < " + self.__bookMaxPrice
+            flagIfFirst += 1
+            if flagIfFirst is 1:
+                self.__selSQL += " Where "
+            self.__selSQL += "bookPrice < " + self.__bookMaxPrice
         if self.__publishTime != 'None':  # 出版时间
-            self.__selSQL += " AND bookCreateTime =" + self.__publishTime
+            flagIfFirst += 1
+            if flagIfFirst is 1:
+                self.__selSQL += " Where "
+            if previousNum >= 2:
+                self.__selSQL += " AND "
+            self.__selSQL += "bookCreateTime =" + self.__publishTime
         if self.__bookAuthor != 'None':  # 作者不为空，模糊查询
-            self.__selSQL += " AND bookAuthor LIKE '%" + self.__bookAuthor + "%'"
+            flagIfFirst += 1
+            if flagIfFirst is 1:
+                self.__selSQL += " Where "
+            if previousNum >= 2:
+                self.__selSQL += " AND "
+            self.__selSQL += "bookAuthor LIKE '%" + self.__bookAuthor + "%'"
         if self.__bookPublisher != 'None':  # 出版社不为空，模糊查询
-            self.__selSQL += " AND bookPublisher LIKE '%" + self.__bookAuthor + "%'"
+            flagIfFirst += 1
+            if flagIfFirst is 1:
+                self.__selSQL += " Where "
+            if previousNum >= 2:
+                self.__selSQL += " AND "
+            self.__selSQL += "bookPublisher LIKE '%" + self.__bookAuthor + "%'"
 
         if self.__sortKey == "书名":  # 按书名首字母[汉字拼音首字母]排序
             self.__selSQL += "ORDER BY bookName"
